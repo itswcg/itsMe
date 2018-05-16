6197fc1fac94967b58b207437de47a65ce8bba9c
itswcg <itswcg@gmail.com>
2018-05-16 14:57:16 +0800

    feed

--- itsMe/authentication/forms.py
+++ itsMe/authentication/forms.py
@@ -1,6 +1,13 @@
 from django import forms
 from django.contrib.auth.models import User
 from captcha.fields import CaptchaField
+from django.contrib.auth.forms import AuthenticationForm
+
+
+class SignInForm(AuthenticationForm):
+    captcha = CaptchaField(label='验证码')
+
+
 
 
 class SignUpForm(forms.ModelForm):
